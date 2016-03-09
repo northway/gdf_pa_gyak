dc1d0adb7816ede76254e0925bd20c062e4d7157
northway <laszlo.attila@me.com>
2016-03-09 21:00:25 +0100

    Init PA gyak commit

---
+++ src/gyak01/main.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gyak01;
+
+/**
+ *
+ * @author northway
+ */
+public class main {
+
+    public static void main(String[] args) {
+        
+        // 1
+        System.out.println("Hello Vilag!");
+        
+        // 3
+        System.out.println("Hello " + "Vilag!");
+        
+        // 4
+        System.out.print("Hello ");
+        System.out.println("Vilag!");
+        
+        // 5
+        System.out.println("Itt vagyok!");
+        
+        // 6
+        System.out.println("Atila, Mernok info, 2015");
+        
+        // 7
+        System.out.println("Attila,");
+        System.out.println("Mernok info,");
+        System.out.println("2015,");
+        
+        // 8
+        System.out.println("Versek itt, versek ott");
+        System.out.println("");
+        System.out.println("Versek itt, versek ott");
+        
+        // 10
+        System.out.println("hat + hat = " + (6+6));
+        
+        // 11
+        System.out.println(9-3);
+        System.out.println(9/-2.0);
+        System.out.println(8*2.5);
+        System.out.println(9%-2);
+        System.out.println(-9%2);
+        System.out.println(9%2);
+        System.out.println(4+3*5);
+        System.out.println((4+3)*5);
+        System.out.println(9/2);
+        System.out.println(9/-2);
+        
+        // 12        
+        System.out.println("Byte: " + Byte.SIZE +" "+ Byte.MIN_VALUE +" "+ Byte.MAX_VALUE);
+        System.out.println("Short: " + Short.SIZE +" "+ Short.MIN_VALUE +" "+ Short.MAX_VALUE);
+        System.out.println("Integer: " + Integer.SIZE +" "+ Integer.MIN_VALUE +" "+ Integer.MAX_VALUE);
+        System.out.println("Long: " + Long.SIZE +" "+ Long.MIN_VALUE +" "+ Long.MAX_VALUE);
+        System.out.println("Double: " + Double.SIZE +" "+ Double.MIN_VALUE +" "+ Double.MAX_VALUE);
+        System.out.println("Float: " + Float.SIZE +" "+ Float.MIN_VALUE +" "+ Float.MAX_VALUE);
+        
+        // 13
+        double a = 16L;
+        double b = 1E6;
+        
+        System.out.println("16L = " + a);
+        System.out.println("1E6 = " + b);
+    }
+    
+}
---
+++ src/gyak02/main.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gyak02;
+
+/**
+ *
+ * @author northway
+ */
+public class main {
+
+    public static void main(String[] args) {
+        
+        // 1
+//        int x,y;
+        
+        // 4
+//        int x = 12, y = 3;
+
+        // 5
+        final int x = 12, y = 3;
+        
+//        x = 12;
+//        y = 3;
+        
+        System.out.println(x +"+"+ y +"="+ (x+y));
+        System.out.println(x +"-"+ y +"="+ (x-y));
+        System.out.println(x +"*"+ y +"="+ (x*y));
+        System.out.println(x +"/"+ y +"="+ (x/y));
+        
+        // 2
+//        y = 5;
+        int z;
+        
+        System.out.println("z="+x+"+"+y+"+"+(z=x+y));
+        
+        // 6
+        double v = 2.5;
+        System.out.println(z/v);
+        
+        // 7
+//        int a,b,c;
+//        
+//        System.out.print("a: ");
+//        a = extra.Console.readInt(); 
+//        System.out.print("b: ");
+//        b = extra.Console.readInt(); 
+//        System.out.print("c: ");
+//        c = extra.Console.readInt();
+//
+//        System.out.println("Legkisebb: " + Math.min(Math.min(a, b), c));
+//        System.out.println("Legnagyobb: " + Math.max(Math.max(a, b), c));
+        
+        // 8
+//        double alap, lab;
+//        
+//        System.out.print("Százalékalap: ");
+//        alap = extra.Console.readDouble();
+//        System.out.print("Százalékláb: ");
+//        lab = extra.Console.readDouble();
+//        
+//        System.out.println("Százalékérték: " + alap*lab/100);
+        
+        // 9
+//        double betet, futam, kamat;
+//        
+//        System.out.print("Betét: ");
+//        betet = extra.Console.readDouble();
+//        System.out.print("Futam: ");
+//        futam = extra.Console.readDouble();
+//        System.out.print("Kamat: ");
+//        kamat = extra.Console.readDouble();
+//        
+//        System.out.println("KamaKama: " + Math.pow(betet*(1+kamat/100), futam));
+
+        // 10
+//        int a,b;
+//        
+//        System.out.print("a: ");
+//        a = extra.Console.readInt(); 
+//        System.out.print("b: ");
+//        b = extra.Console.readInt();
+//        
+//        System.out.println("x=" + (-b/a));
+
+        // 11
+        
+    }
+    
+}
